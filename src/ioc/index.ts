b4dbc004df7b2c4ef99b28d367cd7afcdc0dffb7
Kirill Vasenin <vasenink@ae.com>
2023-06-08 11:32:36 +0300

    DI-001 project updated and created new rep

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,16 @@
 import IoCContainer from 'ioc-lite';
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from '../services/logger';
+import { HTTP } from '../services/http';
+import { Users } from '../services/users';
 
+// using a generic type allows you to control which resources can be registered.
 export const createIoCContainer = () =>  {
   const ioc = new IoCContainer();
   // you can register some resources right now below...
-
+  ioc.registerClass('http', HTTP);
+  ioc.registerClass('logger', Logger);
+  ioc.registerClass('users', Users);
+  
   return ioc;
 };
